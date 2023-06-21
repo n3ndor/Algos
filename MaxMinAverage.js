86355d3b907533affdd2e1f8de6d06986b42c93f
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-06-21 09:53:40 -0400

    singly linked list: max min average

---
+++ MaxMinAverage.js
@@ -0,0 +1,110 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    addFront(value) {
+        let newNode = new Node(value);
+        if (this.head) {
+            newNode.next = this.head;
+        }
+        this.head = newNode;
+        return this.head;
+    }
+
+    contains(value) {
+        let runner = this.head;
+        while (runner !== null) {
+            if (runner.data === value) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    length() {
+        let runner = this.head;
+        let count = 0;
+        while (runner !== null) {
+            count++;
+            runner = runner.next;
+        }
+        console.log(count);
+    }
+
+    display() {
+        let runner = this.head;
+        let values = [];
+        while (runner !== null) {
+            values.push(runner.data);
+            runner = runner.next;
+        }
+        console.log(values.join(', '));
+    }
+
+    max() {
+        if (!this.head) {
+            return null;
+        }
+
+        let maxVal = this.head.data;
+        let runner = this.head.next;
+        while (runner !== null) {
+            if (runner.data > maxVal) {
+                maxVal = runner.data;
+            }
+            runner = runner.next;
+        }
+        return console.log("max:", maxVal)
+    }
+
+    min() {
+        if (!this.head) {
+            return null;
+        }
+
+        let minVal = this.head.data;
+        let runner = this.head.next;
+        while (runner !== null) {
+            if (runner.data < minVal) {
+                minVal = runner.data;
+            }
+            runner = runner.next;
+        }
+        return console.log("min:", minVal)
+    }
+
+    average() {
+        if (!this.head) {
+            return null;
+        }
+
+        let sum = 0;
+        let count = 0;
+        let runner = this.head;
+        while (runner !== null) {
+            sum += runner.data;
+            count++;
+            runner = runner.next;
+        }
+        return console.log("average:", sum / count)
+    }
+}
+
+let SLL1 = new SLL()
+SLL1.addFront(76)
+SLL1.addFront(2)
+SLL1.display()
+SLL1.addFront(11.41)
+SLL1.display()
+SLL1.max()
+SLL1.min()
+SLL1.average()
